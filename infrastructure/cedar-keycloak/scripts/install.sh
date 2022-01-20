df77e81f7b47b89773c0b74e55bf30cef97bc3ed
Timotht Redmond <tredmond@fastmail.com>
2022-01-20 11:23:10 -0800

    Fixed keycloak issue.  It was simply a problem stemming from their update to mysql.   They updated mysql and the key presumably stopped working.

--- infrastructure/cedar-keycloak/scripts/install.sh
+++ infrastructure/cedar-keycloak/scripts/install.sh
@@ -8,10 +8,12 @@ waitForConnection()  {
     done
 }
 
+microdnf -y update
+
 microdnf -y install java-11-openjdk-headless
 alternatives --set java java-11-openjdk.x86_64
 
 microdnf -y install dnf
-dnf -y install ${KEYCLOAK_CONFIG_DIR}/mysql80-community-release-el8-2.noarch.rpm
+dnf -y install ${KEYCLOAK_CONFIG_DIR}/mysql80-community-release-el8-3.noarch.rpm
 dnf -y install mysql
 
